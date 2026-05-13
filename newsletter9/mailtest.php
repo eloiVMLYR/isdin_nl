a67058b2882df6c9971fe08157725e319921a88d
Eloi Vaqué <eloiv@lliures.cat>
2026-05-13 09:27:50 +0000

    newsletter9

--- newsletter9/mailtest.php
+++ newsletter9/mailtest.php
@@ -10,7 +10,7 @@ $from = "eloiv@lliures.cat";
 $fromName = "Eloi Vaqué";
 
 // Carreguem l'HTML
-$htmlFile = "newsletter8.html";
+$htmlFile = "newsletter9.html";
 if (!file_exists($htmlFile)) {
     die("Error: no es troba el fitxer HTML $htmlFile");
 }
