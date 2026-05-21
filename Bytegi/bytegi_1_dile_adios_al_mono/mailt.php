f6a2c1d5a92911f899ad5877946cd34836b0e3db
Eloi Vaqué <eloiv@lliures.cat>
2026-05-21 16:40:23 +0000

    Bytegi nl1

---
+++ Bytegi/bytegi_1_dile_adios_al_mono/mailt.php
@@ -0,0 +1,3 @@
+<?php
+
+var_dump(mail("eloiv@lliures.cat", "test", "test"));
--- Bytegi/bytegi_1_dile_adios_al_mono/mailtest.php
+++ Bytegi/bytegi_1_dile_adios_al_mono/mailtest.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 
 $to = "eloi.vaque@vml.com";
 $to = "eloiv@lliures.cat";
-$subject = "Prueba Newsletter ISDIN";
+$subject = "Prueba Newsletter Bytegi";
 $from = "eloiv@lliures.cat";
 //$from = "eloi.vaque@vml.com";
 $fromName = "Eloi Vaqué";
