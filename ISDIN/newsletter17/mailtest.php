4831d880acd77273f469efe6c4ab9ca2167ce299
Eloi Vaqué <eloiv@lliures.cat>
2026-06-15 11:03:34 +0000

    newsletter 17

--- ISDIN/newsletter17/mailtest.php
+++ ISDIN/newsletter17/mailtest.php
@@ -10,7 +10,7 @@ $from = "eloiv@lliures.cat";
 $fromName = "Eloi Vaqué";
 
 // Carreguem l'HTML
-$htmlFile = "newsletter14.html";
+$htmlFile = "newsletter17.html";
 if (!file_exists($htmlFile)) {
     die("Error: no es troba el fitxer HTML $htmlFile");
 }
